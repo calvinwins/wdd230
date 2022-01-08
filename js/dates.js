29f43bd85d62275759c39ad17866d96dfbc1e0e2
calvinwins <weaver95@byui.edu>
2022-01-08 16:08:31 -0600

    changed colors and allignment

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last modified: ${document.lastModified}`;
+const year = document.querySelector('#year')
+year.textContent = document.year
+
