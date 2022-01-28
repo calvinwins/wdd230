61658b7117d1dafb47797c0cf74460277fe18957
calvinwins <weaver95@byui.edu>
2022-01-28 14:18:30 -0600

    logos, images, stuff

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -1,7 +1,6 @@
 const date =  new Date();
 const year = date.getFullYear();
 
-document.getElementById('currentDate').textContent = date
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = `Last modified: ${document.lastModified}`;
 
