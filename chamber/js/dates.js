c32c00ce1754f370e7b4d82dc9bc98388f204f9d
calvinwins <weaver95@byui.edu>
2022-01-28 16:16:07 -0600

    changes

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -4,4 +4,11 @@ const year = date.getFullYear();
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = `Last modified: ${document.lastModified}`;
 
+const today = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {
+	dateStyle: "full"}).format(today);
+
+document.getElementById('currentDate').innerHTML = `<em>${fulldate}</em>`;
+
 document.getElementById('currentYear').textContent = year;
+
