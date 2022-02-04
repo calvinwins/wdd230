931bf80601aa4a060cff151d3617f540b695356a
calvinwins <weaver95@byui.edu>
2022-02-04 11:55:31 -0600

    changes

--- bom/js/bom.js
+++ bom/js/bom.js
@@ -3,8 +3,8 @@ const buildButton = document.querySelector('button');
 const list = document.getElementById('list')
 
 buildButton.addEventListener('click', () => {
-     let size = list.value;
-     size = '';
+     let size = input.value;
+     input.value = '';
      let li = document.createElement('li');
      let span = document.createElement('span');
      let button = document.createElement('button');
