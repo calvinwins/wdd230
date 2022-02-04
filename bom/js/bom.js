d47892adcb20877bb334bfe03f62048c9cf7c921
calvinwins <weaver95@byui.edu>
2022-02-04 11:48:07 -0600

    changes

--- bom/js/bom.js
+++ bom/js/bom.js
@@ -1,10 +1,23 @@
-const listsize = document.querySelector('#listsize ');
+const input = document.querySelector('input');
 const buildButton = document.querySelector('button');
 const list = document.getElementById('list')
 
 buildButton.addEventListener('click', () => {
      let size = listsize.value;
+     size = '';
      let li = document.createElement('li');
-     li.innerhtml
-     list.appendChild(li)
+     let span = document.createElement('span');
+     let button = document.createElement('button');
+
+     li.appendChild(span);
+     span.textContent = size;
+     li.appendChild(button);
+     button.textContent = '❌';
+     list.appendChild(li);
+
+     button.addEventListener('click', () => {
+         list.removeChild(li);
+     })
+
+     input.focus();
 });
