2c76c260a7da8b1e8ba619bc7dc49d9ce9e68b1b
calvinwins <weaver95@byui.edu>
2022-02-05 21:35:55 -0600

    asd

--- chamber/js/buttons.js
+++ chamber/js/buttons.js
@@ -1,12 +1,9 @@
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation');
 
-menubutton.addEventListener('click', () => 
-{mainnav.classList.toggle('responsive');
-ham.classList.toggle('hidden')}, false)
-;
+menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) 
     mainnav.classList.remove('responsive')
-    ham.classList.remove("hidden");
+    menu.classList.remove("hidden");
 };
