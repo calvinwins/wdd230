0cba6f6833ee0788faaac8b940001028e267369e
calvinwins <weaver95@byui.edu>
2022-02-11 14:37:32 -0600

    6

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,7 +1,7 @@
-const temp = document.getElementById('temp');
-const speed = document.getElementById('windSpeed');
+const temp = document.querySelector('#temp');
+const speed = document.querySelector('#windSpeed');
 
-const windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16));
+const windChill= Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
 
-const roundedWindChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= roundedWindChill;
+document.getElementById("windChill").innerHTML= windChill;   
+
