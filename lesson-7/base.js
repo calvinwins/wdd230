b91d5f3ca66ec82182ca07f53351506c5970bdca
calvinwins <weaver95@byui.edu>
2022-02-15 15:55:59 -0600

    l

---
+++ lesson-7/base.js
@@ -0,0 +1,29 @@
+let imagestoLoad = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+        });
+    }, imageOptions);  
+
+    imagestoLoad.forEach((img) => {
+        imgObserver.observe(img);
+    })
+}
+else {
+    imagestoLoad.forEach((img) => {
+        loadImages(img);
+    })
+}
