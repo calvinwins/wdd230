c6b25322efc901c3015fc43aedd1f6e05d3ddd0b
calvinwins <weaver95@byui.edu>
2022-02-17 11:41:52 -0600

    zxc

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,7 +1,11 @@
 const temp = document.querySelector('#temp');
 const speed = document.querySelector('#windSpeed');
 
-const windChill= Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
-
-document.getElementById("windChill").innerHTML= windChill;   
+if(t <= 50 && s > 3) {
+    const windChill= Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
+    document.getElementById("windChill").innerHTML= windChill;   
+}
+else {
+    document.getElementById("windChill").innerHTML = "N/A"
+}
 
