8dd5dc01ae9e87ffd2c6e4dfca6199e401f1d608
calvinwins <weaver95@byui.edu>
2022-03-02 14:30:42 -0600

    asd

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -7,29 +7,29 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets);  // temporary checking for valid response and data parsing
+    prophets.forEach(displayProphets);  // loop through each prophet
   });
 
   
 
-
+// assign values of each prophet
   function displayProphets(prophet) {
     let card = document.createElement('section');
 
-    //h2 name
+    //name
     let h2 = document.createElement('h2');
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
     card.appendChild(h2);
   
-    //p birth date
-    let p1 = document.createElement('p');
-    p1.textContent = `Date of Birth: ${prophet.birthdate}`;
-    card.appendChild(p1);
+    //Date of Birth
+    let db = document.createElement('p');
+    db.textContent = `Date of Birth: ${prophet.birthdate}`;
+    card.appendChild(db);
   
-    //p birth place
-    let p2 = document.createElement('p');
-    p2.textContent = `Place of birth: ${prophet.birthplace}`;
-    card.appendChild(p2);
+    //PLace of Birth
+    let pb = document.createElement('p');
+    pb.textContent = `Place of birth: ${prophet.birthplace}`;
+    card.appendChild(pb);
   
     //img
     let image = document.createElement('img');
