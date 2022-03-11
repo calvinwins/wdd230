57bca6ef2f32e9f2afbd805e862812db7ce88c0a
calvinwins <weaver95@byui.edu>
2022-03-11 14:11:53 -0600

    we

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,7 +1,26 @@
+
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=50284db3b3fff6d7bb78ac4311f561e2'
+
 
 fetch(apiURL)
     .then((response) => response.json())
-    .then((jsObject) => {
-      console.log(jsObject);
-});
+    .then((x) => {
+      
+      const currentTemp = document.querySelector('#current-temp');
+      const weathericon = document.querySelector('#weathericon');
+      const caption = document.querySelector('figcaption')
+
+      currentTemp.textContent = x.main.temp.toFixed(0);
+      let imgsrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+      let imgalt = x.weather[0].description;
+      imgalt = imgalt.split(' ').map(capitalize).join(' ');
+      currentTemp.textContent = x.main.temp.toFixed(0);
+      weathericon.setAttribute('src', imgsrc);
+      weathericon.setAttribute('alt', imgalt);
+      caption.innerHTML = `currently: ${imgalt}`;
+
+    });
+
+function capitalize(word) {
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
+}
