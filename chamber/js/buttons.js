892f508c25d89b07a110c4cb472966ecd6fb1083
calvinwins <weaver95@byui.edu>
2022-03-16 16:51:03 -0500

    button

--- chamber/js/buttons.js
+++ chamber/js/buttons.js
@@ -3,13 +3,11 @@ const mainnav = document.querySelector('.navigation');
 
 menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-//  const joinButton = document.querySelector('.joinch');
-//  joinButton.addEventListener('click', function() {
-//     window.location='https://calvinwins.github.io/wdd230/chamber/join.html';
-//   });
-//   const joinButtons = document.querySelector('.joinch2');
-//   joinButtons.addEventListener('click', function() {
-//      window.location='https://calvinwins.github.io/wdd230/chamber/join.html';
-//    });
-
-
+ const joinButton = document.querySelector('.joinch');
+ joinButton.addEventListener('click', function() {
+    window.location='https://calvinwins.github.io/wdd230/chamber/join.html';
+  });
+  const joinButtons = document.querySelector('.joinch2');
+  joinButtons.addEventListener('click', function() {
+     window.location='https://calvinwins.github.io/wdd230/chamber/join.html';
+    });
