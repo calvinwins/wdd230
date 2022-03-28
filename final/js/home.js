15849882a374084220667c3fc0fa9a07bbceefd9
calvinwins <weaver95@byui.edu>
2022-03-28 09:30:03 -0500

    changes

--- final/js/home.js
+++ final/js/home.js
@@ -6,11 +6,26 @@ fetch(apiURL)
     .then((x) => {
       
       const currentTemp = document.querySelector('#current-temp');
+    //   const temp1 = document.querySelector('#temp1');
+    //   const temp2 = document.querySelector('#temp1');
+    //   const temp3 = document.querySelector('#temp1');
       const weathericon = document.querySelector('#weathericon');
-      const caption = document.querySelector('figcaption')
-      const currentHumidity = document.querySelector('#humidity')
+    //   const weathericon2 = document.querySelector('#weathericon2');
+    //   const weathericon3 = document.querySelector('#weathericon3');
+    //   const weathericon4 = document.querySelector('#weathericon4');
+      const caption = document.querySelector('figcaption');
+    //   const caption2 = document.querySelector('#figcaption2');
+    //   const caption3 = document.querySelector('#figcaption3');
+    //   const caption4 = document.querySelector('#figcaption4')
+      const currentHumidity = document.querySelector('#humidity');
+    //   const Humidity2 = document.querySelector('#humidity2');
+    //   const Humidity3 = document.querySelector('#humidity3');
+    //   const Humidity4 = document.querySelector('#humidity4');
       const temp = x.current.temp.toFixed(0)
       const humidity = x.current.humidity.toFixed(0);
+    //   const currentAlert = document.querySelector('#alert')
+    //   const tempTomorrow = x.daily.temp.day.toFixed(0)
+      
 
       currentHumidity.textContent = humidity;
       currentTemp.textContent = temp;
@@ -20,5 +35,11 @@ fetch(apiURL)
       weathericon.setAttribute('src', imgsrc);
       weathericon.setAttribute('alt', imgalt);
       caption.innerHTML = `Currently: ${imgalt}`;
+      
+
+    //   let alertName = x.alerts.event;
+    //   let alertStart = x.alerts[0].start;
+    //   let alertEnd = x.alerts[0].end;
+    //   currentAlert.textContent = alertName,alertStart,alertEnd;
 
     });
