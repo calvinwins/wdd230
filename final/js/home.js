a9314132a0d2b9b599b9c0b744b10c164b1d16bf
calvinwins <weaver95@byui.edu>
2022-03-28 08:33:59 -0500

    asd

--- final/js/home.js
+++ final/js/home.js
@@ -1,5 +1,5 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=46.7167492&lon=-101.0540921&units=imperial&appid=a1a0a55f931ddc473d7fe806f1504796'
-const apiURLF = 'https://api.openweathermap.org/data/2.5/forecast?lat=46.7167492&lon=-101.0540921&units=imperial&appid=a1a0a55f931ddc473d7fe806f1504796'
+const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=46.7167492&lon=-101.0540921&exclude=minutely,hourly&units=imperial&appid=a1a0a55f931ddc473d7fe806f1504796'
+
 //current weather
 fetch(apiURL)
     .then((response) => response.json())
@@ -9,28 +9,16 @@ fetch(apiURL)
       const weathericon = document.querySelector('#weathericon');
       const caption = document.querySelector('figcaption')
       const currentHumidity = document.querySelector('#humidity')
-      const temp = x.main.temp.toFixed(0)
-      const humidity = x.main.humidity.toFixed(0);
+      const temp = x.current.temp.toFixed(0)
+      const humidity = x.current.humidity.toFixed(0);
 
       currentHumidity.textContent = humidity;
       currentTemp.textContent = temp;
-      let imgsrc = `images/${x.weather[0].icon}.png`;
-      let imgalt = x.weather[0].description;
-      imgalt = imgalt.split(' ').map(capitalize).join(' ');
+      let imgsrc = `images/${x.current.weather[0].icon}.png`;
+      let imgalt = x.current.weather[0].description;
+    //   imgalt = imgalt.split(' ').map(capitalize).join(' ');
       weathericon.setAttribute('src', imgsrc);
       weathericon.setAttribute('alt', imgalt);
       caption.innerHTML = `Currently: ${imgalt}`;
 
     });
-//forecast
-fetch(apiURLf)
-.then((response) => response.json())
-.then((x) => {
-
-});
-    
-
-function capitalize(word) {
-    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
-
-}
