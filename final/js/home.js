2b07c70b0aa8d6186a2af15096dd3d9643725568
calvinwins <weaver95@byui.edu>
2022-03-29 06:46:32 -0500

    asd]

--- final/js/home.js
+++ final/js/home.js
@@ -6,25 +6,17 @@ fetch(apiURL)
     .then((x) => {
       
       const currentTemp = document.querySelector('#current-temp');
-    //   const temp1 = document.querySelector('#temp1');
-    //   const temp2 = document.querySelector('#temp1');
-    //   const temp3 = document.querySelector('#temp1');
+
       const weathericon = document.querySelector('#weathericon');
-    //   const weathericon2 = document.querySelector('#weathericon2');
-    //   const weathericon3 = document.querySelector('#weathericon3');
-    //   const weathericon4 = document.querySelector('#weathericon4');
+      
+
       const caption = document.querySelector('figcaption');
-    //   const caption2 = document.querySelector('#figcaption2');
-    //   const caption3 = document.querySelector('#figcaption3');
-    //   const caption4 = document.querySelector('#figcaption4')
+
       const currentHumidity = document.querySelector('#humidity');
-    //   const Humidity2 = document.querySelector('#humidity2');
-    //   const Humidity3 = document.querySelector('#humidity3');
-    //   const Humidity4 = document.querySelector('#humidity4');
+
       const temp = x.current.temp.toFixed(0)
       const humidity = x.current.humidity.toFixed(0);
-    //   const currentAlert = document.querySelector('#alert')
-    //   const tempTomorrow = x.daily.temp.day.toFixed(0)
+
       
 
       currentHumidity.textContent = humidity;
@@ -43,19 +35,19 @@ fetch(apiURL)
     //   currentAlert.textContent = alertName,alertStart,alertEnd;
 
     });
+    
+    fetch(apiURL)
+    .then((response) => response.json())
+    .then(function (x) {
+      const daily = jsonObject['daily'];
+      daily.slice(-3).forEach(displayDaily);  // loop through each business
+    });
+    function display(day) {
 
-    // response.json().then(function (data) {
-    //     var fday = "";
-    //     data.daily.forEach((value, index) => {
-    //         if (index > 0) {
-    //             var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
-    //                 weekday: "long",
-    //             });
-    //             var icon = value.weather[0].icon;
-    //             var temp = value.temp.day.toFixed(0);
-    //             fday = `<div class="forecast-day">
-    //                 <p>${dayname}</p>
-    //                 <p><span class="ico-${icon}" title="${icon}"></span></p>
-    //                 <div class="forecast-day--temp">${temp}<sup>°C</sup></div>
-    //             </div>`;
-    //             forecastEl[0].insertAdjacentHTML('beforeend', fday);
+      let weatherimg = `images/${x.daily.weather[0].icon}.png`;
+      let weatheralt = x.daily.weather[0].description;
+    
+      let temperature = x.daily.weather.temp.toFixed(0);
+    
+      let humidity = x.daily.weather.humidity.toFIxed(0);
+    }
